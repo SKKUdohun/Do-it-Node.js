c5f6846b25da6c06a2cedad8a85ea201ab4acf1a
SKKUdohun <eclipsekd@naver.com>
2017-08-24 00:39:45 +0900

    170824 00:39

---
+++ ExpressExample/app8.js
@@ -0,0 +1,47 @@
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+// Express 미들웨어 불러오기
+var bodyParser = require('body-parser')
+  , static = require('serve-static');
+
+//익스프레스 객체 생성
+var app = express();
+
+//라우터 객체 참조
+var router = express.Router();
+
+// 라우팅 함수 등록
+router.route('/process/login').post(function(req,res){
+  console.log('/process/login 처리함.');
+
+  var paramId = req.body.id || req.query.id;
+  var paramPassword = req.body.password || req.query.password
+
+  res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+  res.write('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+  res.write('<div><p>Param id : ' + paramId + '</p></div>');
+  res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
+  res.write("<br><br><a href='/public/login2.html'>로그인 페이지로 돌아가기</a>");
+  res.end();
+})
+//라우터 객체를 app 객체에 등록
+app.use('/',router);
+
+//기본 속성 설정
+app.set('port', process.env.PORT||3000);
+
+//body-parser를 사용해 application/x-www-form-urlencoded 파싱
+app.use(bodyParser.urlencoded({extended:false}));
+
+//body-parser를 사용해 application/json 파싱
+app.use(bodyParser.json());
+
+//./public 폴더로 접근가능하게 만듬
+app.use(static(path.join(__dirname,'public')));
+
+
+http.createServer(app).listen(3000, function(){
+  console.log('익스프레스 서버를 3000번 포트에서 시작했습니다. ');
+});
